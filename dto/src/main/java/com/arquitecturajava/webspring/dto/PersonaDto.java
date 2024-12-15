ff4f5976ceeed47c7bcef6dd70d1a7b133d420fc
Eduardo <33epico@gmail.com>
2024-12-15 21:25:22 +0100

    inicio

---
+++ dto/src/main/java/com/arquitecturajava/webspring/dto/PersonaDto.java
@@ -0,0 +1,23 @@
+package com.arquitecturajava.webspring.dto;
+
+public class PersonaDto {
+
+	private String nombre;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public PersonaDto(String nombre) {
+		super();
+		this.nombre = nombre;
+	}
+	
+	
+	
+	
+}
---
+++ dto/src/main/java/com/arquitecturajava/webspring/mappers/PersonaMapper.java
@@ -0,0 +1,19 @@
+package com.arquitecturajava.webspring.mappers;
+
+import com.arquitecturajava.webspring.dto.PersonaDto;
+import com.arquitecturajava.webspring.models.Persona;
+
+
+public class PersonaMapper {
+	
+	public static PersonaDto toDto(Persona persona) {
+		return new PersonaDto(persona.getNombre());
+	}
+	
+	public static Persona toBo(PersonaDto personaDto) {
+		return new Persona(personaDto.getNombre());
+	}
+	
+	
+
+}
